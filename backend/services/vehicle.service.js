737d94dd408762e520fa2029b27048868add7321
Tesfamichael12 <tesfamichael132@gmail.com>
2025-07-07 04:34:06 +0300

    fix(vehicle): correct SQL query for fetching vehicle by serial number

--- backend/services/vehicle.service.js
+++ backend/services/vehicle.service.js
@@ -2,7 +2,8 @@ const {query}=require("../config/db.config");
 const { get } = require("../routes/vehicle.routes");
 
 async function getVehicleBySerial(serial){
-    const sql="SELECT * FROM customer_vehicle_info WHERE vehicle_serial=?";
+    //here edit the sql as per like i added active_vehicle colunm so what you check from the database is that the vehicle is active or not so like change the sql to consider that the active status mustb 1 to compare by serial number i dont want vehicle info if the active status of that vehicle is 0
+    const sql="SELECT * FROM customer_vehicle_info WHERE vehicle_serial=? AND active_vehicle=1";
     const [rows]=await query(sql,serial);
     return rows;
 }
@@ -20,13 +21,14 @@ async function createVehicle(vehicle){
             vehicle_mileage, 
             vehicle_tag, 
             vehicle_serial, 
-            vehicle_color
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+            vehicle_color,
+            active_vehicle
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?);
     `;
 
-    const {customer_id, vehicle_year, vehicle_make, vehicle_model, vehicle_type, vehicle_mileage, vehicle_tag, vehicle_serial, vehicle_color} = vehicle;
+    const {customer_id, vehicle_year, vehicle_make, vehicle_model, vehicle_type, vehicle_mileage, vehicle_tag, vehicle_serial, vehicle_color,active_vehicle} = vehicle;
 
-    const rows = await query(sql, [customer_id, vehicle_year, vehicle_make, vehicle_model, vehicle_type, vehicle_mileage, vehicle_tag, vehicle_serial, vehicle_color]);
+    const rows = await query(sql, [customer_id, vehicle_year, vehicle_make, vehicle_model, vehicle_type, vehicle_mileage, vehicle_tag, vehicle_serial, vehicle_color,active_vehicle]);
 
     if(rows.affectedRows === 1){
         return true;
