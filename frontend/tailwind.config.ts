b1320c0d9584bf151fb47522e9fb763ee7d5c824
Tesfamichael12 <tesfamichael132@gmail.com>
2025-07-10 12:34:00 +0300

    feat(ui): implement initial homepage design and layout

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'banner': "url('/images/banner1.jpg')",
+        'vban1':"url('/images/vban1.jpg')",
+        'vban2':"url('/images/vban2.jpg')",
+        'patternBg':"url('/images/pattern.png')",
+      },
+      colors:{
+        customBlue:"#08194A",
+        customeRed:"#EE0D09"
+        
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
