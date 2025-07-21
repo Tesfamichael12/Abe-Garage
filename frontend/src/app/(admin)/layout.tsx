81a955457cd7f025359afadc747ba46903f26d07
Tesfamichael Tafere <tesfamichael132@gmail.com>
2025-07-21 07:07:31 +0300

    refactor(frontend): replace AdminMenu with AdminLayoutClient to solve the issue Client Component hook running on the server

--- frontend/src/app/(admin)/layout.tsx
+++ frontend/src/app/(admin)/layout.tsx
@@ -1,5 +1,4 @@
-"use client";
-import AdminMenu from "@/components/adminMenu/AdminMenu";
+import AdminLayoutClient from "./AdminLayoutClient";
 import React from "react";
 
 export default function AdminLayout({
@@ -7,12 +6,5 @@ export default function AdminLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex bg-gray-100 min-h-screen">
-      <div className="sticky top-[70px] h-[calc(100vh-70px)]">
-        <AdminMenu />
-      </div>
-      <main className="flex-grow p-4 sm:p-6 lg:p-8">{children}</main>
-    </div>
-  );
+  return <AdminLayoutClient>{children}</AdminLayoutClient>;
 }
